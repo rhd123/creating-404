823d794e0f26b6ac780f71793d43d52fced7f644
BrunuhVille <55394446+BrunuhVille@users.noreply.github.com>
2020-11-11 21:56:13 +0800

    Update index.js

--- alpha阶段/小程序部分/bookinfo/index.js
+++ alpha阶段/小程序部分/bookinfo/index.js
@@ -1,10 +1,24 @@
-// 云函数入口文件
 const cloud = require('wx-server-sdk')
-//引入request-promise用于做网络请求
-var rp = require('request-promise');
 cloud.init()
+var rp = require('request-promise');
+exports.main =async  (event, context) => {
 
-// 云函数入口函数
-exports.main = async (event, context) => {
-  return event.isbn;
+  var options = {
+    uri: 'https://jisuisbn.market.alicloudapi.com/isbn/query',
+    qs: {
+        isbn: event.isbn 
+        // -> uri + '?isbn=xxxxx%20xxxxx'
+    },
+    headers: {
+        'User-Agent': 'Request-Promise',
+        'Authorization': 'APPCODE 823fbd610c0b460e91ecd0171714da0b'
+    },
+    json: true 
+  };
+  var res=rp(options).then(html=>{
+      return html;
+    }).catch(err=>{
+      console.log(err);
+    })
+  return res
 }
